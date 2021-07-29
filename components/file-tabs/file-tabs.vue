e4ecbdbbbe49fb105c7b10570da1fc0a97b6dc8f
lif3ng <lif3ng@icloud.com>
2021-07-29 22:58:13 +0800

    file tabs

--- components/file-tabs/file-tabs.vue
+++ components/file-tabs/file-tabs.vue
@@ -1,27 +1,55 @@
 <template>
-	<view>
-		file tabs 2
-		<view v-for="(type,index) in tabs" :key="index" style="display:inline-block" class="px-2">{{type}}</view>
-	</view>
+  <view>
+    file tabs 2
+    <view class="space-x-1">
+      <view
+        v-for="{ key, label } in _tabs"
+        :key="key"
+        style="display: inline-block"
+        :class="[
+          'px-2 rounded-md rounded-b-none',
+          key === active
+            ? 'bg-blue-500 text-white'
+            : 'bg-gray-200 text-gray-700 hover:bg-gray-300 cursor-pointer',
+        ]"
+        @click="$emit('update:active', key)"
+        >{{ label }}</view
+      >
+    </view>
+  </view>
 </template>
 
 <script>
-	export default {
-		name: "file-tabs",
-		props: {
-			tabs: {
-				type: Array,
-				default: () => []
-			}
-		},
-		data() {
-			return {
-
-			};
-		}
-	}
+export default {
+  name: "file-tabs",
+  props: {
+    active: {
+      type: String,
+      default: "",
+    },
+    tabs: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  computed: {
+    _tabs() {
+      return this.tabs.map((tab) => {
+        if (typeof tab === "string") {
+          return {
+            key: tab.toLowerCase(),
+            label: tab,
+          };
+        }
+        return tab;
+      });
+    },
+  },
+  data() {
+    return {};
+  },
+};
 </script>
 
 <style>
-
 </style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,113 +1,119 @@
 <template>
-	<view class="content">
-		<file-tabs :tabs="tabs" />
-		html
-		<textarea v-model="htmlText" />
-		css
-		<textarea v-model="cssText" />
-		<div><button @click="handleSave">save</button>
-			<div v-if="id">id:{{id}}</div>
-			<a :href="linkById" target="_blank">{{linkById}}</a>
-		</div>
-		preview
-		<iframe ref="preview" />
-	</view>
+  <view class="content">
+    <file-tabs :tabs="tabs" :active.sync="activeTab" />
+    <p>{{ activeTab }}</p>
+    html
+    <textarea v-model="htmlText" />
+    css
+    <textarea v-model="cssText" />
+    <div>
+      <button @click="handleSave">save</button>
+      <div v-if="id">id:{{ id }}</div>
+      <a :href="linkById" target="_blank">{{ linkById }}</a>
+    </div>
+    preview
+    <iframe ref="preview" />
+  </view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				tabs: ['html', 'css'],
-				id: '',
-				htmlText: '',
-				cssText: ''
-			}
-		},
-		onLoad() {
-			const id = window.location.pathname.substr(1)
-			if (id) {
-				const db = uniCloud.database();
-				db.collection('snippets').doc(id).get().then(({
-					result: {
-						affectedDocs,
-						data: [item]
-					}
-				}) => {
-					if (affectedDocs === 1) {
-						console.log(item)
-						this.htmlText = item.html
-						this.cssText = item.css
-					}
-				})
-			}
-		},
-		watch: {
-			htmlText(html) {
-				console.log({
-					html
-				})
-				this.generatePreview()
-			},
-			cssText(css) {
-				console.log({
-					css
-				})
-				this.generatePreview()
-			}
-		},
-		computed: {
-			linkById() {
-				if (!this.id) return ''
-				return `/${this.id}`
-			}
-		},
-		methods: {
-			handleSave() {
-				const db = uniCloud.database();
-				db.collection('snippets').add({
-					html: this.htmlText,
-					css: this.cssText
-				}).then(({
-					result: {
-						id
-					}
-				}) => {
-					this.id = id
-				})
-			},
-			generatePreview() {
-				this.$refs.preview.contentDocument.body.innerHTML = `${this.htmlText}
-				<style>${this.cssText}</style>`
-			}
-		}
-	}
+export default {
+  data() {
+    return {
+      tabs: ["HTML", "CSS"],
+      activeTab: "html",
+      id: "",
+      htmlText: "",
+      cssText: "",
+    };
+  },
+  onLoad() {
+    const id = window.location.pathname.substr(1);
+    if (id) {
+      const db = uniCloud.database();
+      db.collection("snippets")
+        .doc(id)
+        .get()
+        .then(
+          ({
+            result: {
+              affectedDocs,
+              data: [item],
+            },
+          }) => {
+            if (affectedDocs === 1) {
+              console.log(item);
+              this.htmlText = item.html;
+              this.cssText = item.css;
+            }
+          }
+        );
+    }
+  },
+  watch: {
+    htmlText(html) {
+      console.log({
+        html,
+      });
+      this.generatePreview();
+    },
+    cssText(css) {
+      console.log({
+        css,
+      });
+      this.generatePreview();
+    },
+  },
+  computed: {
+    linkById() {
+      if (!this.id) return "";
+      return `/${this.id}`;
+    },
+  },
+  methods: {
+    handleSave() {
+      const db = uniCloud.database();
+      db.collection("snippets")
+        .add({
+          html: this.htmlText,
+          css: this.cssText,
+        })
+        .then(({ result: { id } }) => {
+          this.id = id;
+        });
+    },
+    generatePreview() {
+      this.$refs.preview.contentDocument.body.innerHTML = `${this.htmlText}
+				<style>${this.cssText}</style>`;
+    },
+  },
+};
 </script>
 
 <style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		/* align-items: center;
+.content {
+  display: flex;
+  flex-direction: column;
+  /* align-items: center;
 		justify-content: center; */
-	}
+}
 
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
+.logo {
+  height: 200rpx;
+  width: 200rpx;
+  margin-top: 200rpx;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 50rpx;
+}
 
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
+.text-area {
+  display: flex;
+  justify-content: center;
+}
 
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
+.title {
+  font-size: 36rpx;
+  color: #8f8f94;
+}
 </style>
